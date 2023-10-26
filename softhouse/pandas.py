1f98611c89f9b9bebd56180c28b11e3bb75e42a7
Axel Gagge <axel.gagge@fysik.su.se>
2023-10-26 12:59:36 +0200

    Pandas, importing csv

--- softhouse/pandas.py
+++ softhouse/pandas.py
@@ -1,4 +1,14 @@
+from datetime import datetime
 import pandas as pd
 
-df = pd.read_csv("in.csv", sep=";")
+def date_converter(s):
+    return datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
+
+df = pd.read_csv("in.csv", 
+    header=0,  # don't use the provided header
+    names=["date", "code", "price"],  # instead use this one
+    sep=";", 
+    converters={"date": date_converter}  # convert to date object
+)
+
 print(df)
