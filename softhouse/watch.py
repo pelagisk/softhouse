4d119c8014febad827cce5553b443845ed9aef33
Axel Gagge <axel.gagge@fysik.su.se>
2023-10-27 11:08:47 +0200

    investigating watchdog library

--- softhouse/watch.py
+++ softhouse/watch.py
@@ -1,19 +1,16 @@
 import sys
 import time
-import logging
 from watchdog.observers import Observer
-from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
 
-# example of how to use watchdog
+class Event(FileSystemEventHandler):
+    def dispatch(self, event):
+        print("in.csv was modified!")
 
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO,
-                        format='%(asctime)s - %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    path = sys.argv[1] if len(sys.argv) > 1 else '.'
-    event_handler = LoggingEventHandler()
+if __name__ == "__main__":    
+    event_handler = Event()
     observer = Observer()
-    observer.schedule(event_handler, path, recursive=True)
+    observer.schedule(event_handler, "in.csv", recursive=True)
     observer.start()
     try:
         while True:
