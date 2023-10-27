e95bb53f95a15e81fa50c82a2088a643306654d4
Axel Gagge <axel.gagge@fysik.su.se>
2023-10-27 09:17:01 +0200

    Refactored brute force solution

--- softhouse/crunch.py
+++ softhouse/crunch.py
@@ -2,47 +2,58 @@ from datetime import datetime, timedelta
 import pandas as pd
 
 
-# first attempt, brute force solution
-
-
 def date_converter(s):
     return datetime.strptime(s, '%Y-%m-%d %H:%M:%S')
 
 
-df = pd.read_csv("in.csv", 
-    header=0,  # don't use the provided header
-    names=["date", "code", "price"],  # instead use this one
-    sep=";", 
-    converters={"date": date_converter},  # convert to date object
-)
+def find_best_stocks_brute_force(n=3):
+    """A brute force solution to finding the best stocks"""
 
-# find for all labels the increase in price during last 24 hours
-now = df.date.iloc[-1]
-delta = timedelta(days=1)
+    df = pd.read_csv("in.csv", 
+        header=0,  # don't use the provided header
+        names=["date", "code", "price"],  # instead use this one
+        sep=";", 
+        converters={"date": date_converter},  # convert to date object
+    )
 
-codes = df.code.unique()
-stock_info = []
+    # find for all labels the increase in price during last 24 hours
+    now = df.date.iloc[-1]
+    delta = timedelta(days=1)
 
-for code in codes:
-    # updates for this stock
-    updates = df[df.code == code]
+    codes = df.code.unique()
+    stock_info = []
 
-    # latest price
-    latest_price = updates.price.iloc[-1]
+    for code in codes:
+        # updates for this stock
+        updates = df[df.code == code]
 
-    # all updates within last 24 hours
-    updates_last_day = updates[now - updates.date < delta]
-    updates_before = updates[now - updates.date > delta]
+        # latest price
+        latest_price = updates.price.iloc[-1]
 
-    # if it is not updated during the last day, it is constant
-    if len(updates_last_day) == 0:
-        percentage_increase = 0.0               
-    else:  # otherwise, calculate the change
-        old_price = updates_before.price.iloc[-1]
-        change_in_price = latest_price - old_price
-        percentage_increase = 100 * change_in_price / old_price        
+        # all updates within last 24 hours
+        updates_before = updates[now - updates.date > delta]
 
-    stock_info.append((code, percentage_increase, latest_price))
+        # if all updates are made before today, this condition is true
+        # it follows that no updates were made within 24 hours so the stock has 0% change
+        if len(updates_before) == len(df):
+            percentage_increase = 0.0               
+        else:  # otherwise, calculate the change
+            old_price = updates_before.price.iloc[-1]
+            change_in_price = latest_price - old_price
+            percentage_increase = 100 * change_in_price / old_price        
 
-stock_info = sorted(stock_info, key=(lambda line: line[1]), reverse=True)
-print(stock_info[0:3])
+        stock_info.append((code, percentage_increase, latest_price))
+
+    # sort by index 1: the stock price increase 
+    stock_info = sorted(stock_info, key=(lambda line: line[1]), reverse=True)
+
+    # return in the specified format
+    winners = []
+    for i, (name, percent, latest) in enumerate(stock_info[0:n]):
+        winners.append({
+            "rank": i + 1,
+            "name": name,
+            "percent": percent, 
+            "latest": latest,
+        })
+    return {"winners": winners}
