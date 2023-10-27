5ba6772793d7ef5339fd565452c7ce28f5a5929b
Axel Gagge <axel.gagge@fysik.su.se>
2023-10-27 09:59:36 +0200

    Improving readability

--- softhouse/find_best_stocks.py
+++ softhouse/find_best_stocks.py
@@ -9,7 +9,7 @@ def date_converter(s: str):
 def find_best_stocks_brute_force(n=3):
     """A brute force solution to finding the best stocks"""
 
-    df = pd.read_csv("in.csv", 
+    all_updates = pd.read_csv("in.csv", 
         header=0,  # don't use the provided header
         names=["date", "code", "price"],  # instead use this one
         sep=";", 
@@ -17,15 +17,15 @@ def find_best_stocks_brute_force(n=3):
     )
 
     # find for all labels the increase in price during last 24 hours
-    now = df.date.iloc[-1]
+    now = all_updates.date.iloc[-1]
     delta = timedelta(days=1)
 
-    codes = df.code.unique()
+    codes = all_updates.code.unique()
     stock_info = []
 
     for code in codes:
         # updates for this stock
-        updates = df[df.code == code]
+        updates = all_updates[all_updates.code == code]
 
         # latest price
         latest_price = updates.price.iloc[-1]
@@ -34,7 +34,7 @@ def find_best_stocks_brute_force(n=3):
         updates_before = updates[now - updates.date > delta]
 
         # if all updates are made before last 24 hours, this condition is true        
-        if len(updates_before) == len(df):
+        if len(updates_before) == len(all_updates):
             # it follows that no updates were made within 24 hours so the stock has 0% change
             percentage_increase = 0.0               
         else:  # otherwise, calculate the change
