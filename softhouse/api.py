2b289379fb83d69f38bde23391b18e07820e70b8
Axel Gagge <axel.gagge@fysik.su.se>
2023-10-30 13:43:47 +0100

    Documentation

--- softhouse/api.py
+++ softhouse/api.py
@@ -3,32 +3,35 @@ from fastapi import FastAPI
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 
-
-
 from .find_best_stocks import find_best_stocks_brute_force
 from .watch import create_observer
 
+# `best_stocks` is a global variable which should hold our best 3 stocks
+global best_stocks 
 
-global winners 
-winners = find_best_stocks_brute_force() 
-
-def update_winners():
+# it is updated using this function
+def update_best_stocks():
     # TODO logging
-    print("Updating winners")
-    global winners    
-    winners = find_best_stocks_brute_force()    
+    print("Updating best_stocks")
+    global best_stocks    
+    best_stocks = find_best_stocks_brute_force()    
 
-observer = create_observer("in.csv", lambda event: update_winners())
+# input file `in.csv` is watched for changes
+observer = create_observer("in.csv", lambda event: update_best_stocks())
 
+# lifespan of FastAPI app is the following
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    update_winners()
+    # when starting up, update `best_stocks`
+    update_best_stocks()
     yield
+    # when shutting down, remember to shut down observer
     observer.stop()
    
+# initialize app with that lifespan
 app = FastAPI(lifespan=lifespan)
 
-
+# the only route is a GET
 @app.get("/")
 def root():
-    return winners
+    return best_stocks
